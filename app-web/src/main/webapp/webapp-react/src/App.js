c5d5ae4bf304f6b2b8ce1bbfc994fd03bdb96ace
DiegoLentz <diegoolentz@gmail.com>
2022-08-08 16:47:56 -0300

    clase60
    -creacion de componente detalle

--- app-web/src/main/webapp/webapp-react/src/App.js
+++ app-web/src/main/webapp/webapp-react/src/App.js
@@ -1,19 +1,30 @@
 
+import { BrowserRouter,Routes,Route, Link } from 'react-router-dom';
 import styles from './App.module.css';
-import { MovieGird } from './Components/MovieGrid';
+import { MovieGird } from './components/MovieGrid';
+import { MovieDetail } from './components/MovieDetail';
+import { SearchMovie } from './components/SearchMovie';
 
 function App() {
   // console.log(styles);
 
   return (
-    <>
+    <BrowserRouter>
       <header>
-        <h1 className={styles.primerParrafo}>Peliculas</h1>
+        <Link to={'/'}>
+          <div>
+            <h1 className={styles.title}>Peliculas</h1>
+            <SearchMovie/>
+          </div>
+        </Link>
       </header>
       <main>
-        <MovieGird></MovieGird>
+        <Routes>
+          <Route path='/' element={<MovieGird/>}/>
+          <Route path='/detail/:movieId' element={<MovieDetail/>}/>
+        </Routes>
       </main>
-    </>
+    </BrowserRouter>
   );
 }
 
