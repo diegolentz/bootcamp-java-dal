14fa6720a128ac01530194af4d89285c5932f4fe
DiegoLentz <diegoolentz@gmail.com>
2022-08-11 11:36:20 -0300

    clase61

--- app-web/src/main/webapp/webapp-react/src/components/MovieGrid.js
+++ app-web/src/main/webapp/webapp-react/src/components/MovieGrid.js
@@ -2,20 +2,37 @@
 import { MovieCard } from './MovieCard';
 import styles from './MovieGrid.module.css';
 import { useState, useEffect } from 'react';
+import { get } from './../utils/httpClient';
+import { useLocation } from 'react-router-dom';
+import { Spinner } from './Spinner';
+// import { useQuery } from '../hooks/useQuery';
 
 export function MovieGird() {
 
     //estado > steState
     const [peliculas, setPeliculas] = useState([]);
+    const query =  new URLSearchParams(useLocation().search);
+    // const query =  useQuery();
+    const search = query.get("search");
+    const [isLoading,setIsLoading] = useState(true);
 
     //cuando se carga el componente se ejecuta "el que tiene []"
     useEffect( ()=> {
-        //fetch
-        //axios
-        fetch('https://api.themoviedb.org/3/discover/movie?api_key=812a4ea035f5d16fb07e4f12ad70fc60&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate')
-        .then(pepe => pepe.json())
-        .then(data => setPeliculas(data.results));
-    },[]);
+      setIsLoading(true);
+      const urlSerch = search 
+        ? '/search/movie?query='+search
+        : '/discover/movie';
+
+       get(urlSerch)
+       .then(data => {
+          setPeliculas(data.results)
+          setIsLoading(false);
+      });
+    },[search]);
+
+    if(isLoading) {
+      return <Spinner/>
+    }
 
     return (
         <ul className={styles.movieGrid}>
